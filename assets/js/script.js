22e6e9e15f1d002b33a739c7643a4b11e1b6afd8
Mohammed Salah <moeldowy@iti.gov.eg>
2023-12-04 10:18:32 +0200

    a WordPress School

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Custom JavaScript for Abo Salah - WordPress School
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Example: Toggle navigation menu in mobile view
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarMenu = document.querySelector('.navbar-collapse');
+
+    navbarToggler.addEventListener('click', function() {
+        navbarMenu.classList.toggle('show');
+    });
+
+    // Add more interactivity as needed
+});
